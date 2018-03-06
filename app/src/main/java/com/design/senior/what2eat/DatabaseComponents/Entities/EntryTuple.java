bfa52ff91c8b8ed56c616cd199e2d90456edd4f7
knarjp <kevajess@gmail.com>
2018-03-06 09:48:06 -0500

    implemented MealTuple and tests for enum getter and setter system, started work on EntryTuple

--- app/src/main/java/com/design/senior/what2eat/DatabaseComponents/Entities/EntryTuple.java
+++ app/src/main/java/com/design/senior/what2eat/DatabaseComponents/Entities/EntryTuple.java
@@ -1,11 +1,62 @@
 package com.design.senior.what2eat.DatabaseComponents.Entities;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.arch.persistence.room.ForeignKey.CASCADE;
 
 /**
  * Created by KJ on 3/5/2018.
  */
 
-@Entity
+/*@Entity (foreignKeys = @ForeignKey(entity = MealTuple.class,
+                                    parentColumns = "MealID",
+                                    childColumns = "MealID",
+                                    onDelete = CASCADE))*/
 public class EntryTuple {
+    /*@PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "ID", typeAffinity = ColumnInfo.INTEGER)
+    private int ID;
+
+    @NonNull
+    @ColumnInfo(name = "MealID", typeAffinity = ColumnInfo.INTEGER)
+    private int mealID;
+
+    @NonNull
+    @ColumnInfo(name = "Date", typeAffinity = ColumnInfo.TEXT) // STORED IN FORMAT dd/MM/yyyy
+    private String date;
+
+    static final String datePattern = "dd/MM/yyyy";
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getMealID() {
+        return mealID;
+    }
+
+    public void setMealID(int mealID) {
+        this.mealID = mealID;
+    }
+
+    public Date getDate() throws Exception {
+        return DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US).parse(date);
+    }
+
+    public void setDate(Date date) {
+        this.date = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US).format(date);
+    }*/
 }
--- app/src/main/java/com/design/senior/what2eat/DatabaseComponents/Entities/MealTuple.java
+++ app/src/main/java/com/design/senior/what2eat/DatabaseComponents/Entities/MealTuple.java
@@ -5,6 +5,14 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+import com.design.senior.what2eat.DatabaseComponents.Enums.AllergyType;
+import com.design.senior.what2eat.DatabaseComponents.Enums.DietType;
+import com.design.senior.what2eat.DatabaseComponents.Enums.EntryType;
+import com.design.senior.what2eat.DatabaseComponents.Enums.MealTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by KJ on 3/5/2018.
  */
@@ -45,13 +53,19 @@ public class MealTuple {
 
     @NonNull
     @ColumnInfo(name = "MealTime", typeAffinity = ColumnInfo.TEXT)
-    private String MealTime;
+    private String mealTime;
 
     @NonNull
     @ColumnInfo(name = "EntryType", typeAffinity = ColumnInfo.TEXT)
-    private String EntryType;
+    private String entryType;
 
-    @NonNull
+    public MealTuple() {
+        this.allergies = AllergyType.NONE.name() + ",";
+        this.dietType = DietType.NONE.name() + ",";
+        this.mealTime = MealTime.BREAKFAST.name();
+        this.entryType = EntryType.DEFAULT.name();
+    }
+
     public int getMealID() {
         return mealID;
     }
@@ -100,43 +114,112 @@ public class MealTuple {
         this.caloricAmount = caloricAmount;
     }
 
-    // TODO: implement enum return type
-    public String getAllergies() {
-        return allergies;
+    public List<AllergyType> getAllergies() {
+        List<AllergyType> allergiesList = new ArrayList<>();
+
+        if(allergies.contains(AllergyType.NONE.name())) {
+            allergiesList.add(AllergyType.NONE);
+        }
+
+        if(allergies.contains(AllergyType.MILK.name())) {
+            allergiesList.add(AllergyType.MILK);
+        }
+
+        if(allergies.contains(AllergyType.EGGS.name())) {
+            allergiesList.add(AllergyType.EGGS);
+        }
+
+        if(allergies.contains(AllergyType.FISH.name())) { // need to account for FISH and SHELLFISH having the same substring "FISH"
+            if(allergies.startsWith(AllergyType.FISH.name())) { // FISH will either be at start of list
+                allergiesList.add(AllergyType.FISH);
+            } else if (allergies.contains("," + AllergyType.FISH.name())) { // or not, and have a comma before it
+                allergiesList.add(AllergyType.FISH);
+            }
+        }
+
+        if(allergies.contains(AllergyType.SHELLFISH.name())) {
+            allergiesList.add(AllergyType.SHELLFISH);
+        }
+
+        if(allergies.contains(AllergyType.TREENUTS.name())) {
+            allergiesList.add(AllergyType.TREENUTS);
+        }
+
+        if(allergies.contains(AllergyType.PEANUTS.name())) {
+            allergiesList.add(AllergyType.PEANUTS);
+        }
+
+        if(allergies.contains(AllergyType.WHEAT.name())) {
+            allergiesList.add(AllergyType.WHEAT);
+        }
+
+        if(allergies.contains(AllergyType.SOY.name())) {
+            allergiesList.add(AllergyType.SOY);
+        }
+
+        return allergiesList;
     }
 
-    // TODO: implement enum input type
-    public void setAllergies(String allergies) {
-        this.allergies = allergies;
+
+    public void setAllergies(List<AllergyType> allergiesList) {
+        this.allergies = "";
+
+        if(allergiesList.isEmpty()) {
+            this.allergies = "NONE,";
+        } else {
+            for (AllergyType allergy : allergiesList) {
+                this.allergies += allergy.name() + ',';
+            }
+        }
     }
 
-    // TODO: implement enum return type
-    public String getDietType() {
-        return dietType;
+    public DietType getDietType() {
+        if(dietType.contains(DietType.VEGAN.name())) {
+            return DietType.VEGAN;
+        } else if(dietType.contains(DietType.VEGETARIAN.name())) {
+            return DietType.VEGETARIAN;
+        } else if(dietType.contains(DietType.PESCETARIAN.name())) {
+            return DietType.PESCETARIAN;
+        } else if(dietType.contains(DietType.PALEO.name())) {
+            return DietType.PALEO;
+        } else if(dietType.contains(DietType.NONE.name())) {
+            return DietType.NONE;
+        } else {
+            throw new RuntimeException();
+        }
     }
 
-    // TODO: implement enum input type
-    public void setDietType(String dietType) {
-        this.dietType = dietType;
+    public void setDietType(DietType dietType) {
+        this.dietType = dietType.name();
     }
 
-    // TODO: implement enum return type
-    public String getMealTime() {
-        return MealTime;
+    public MealTime getMealTime() {
+        if(mealTime.contains(MealTime.BREAKFAST.name())) {
+            return MealTime.BREAKFAST;
+        } else if(mealTime.contains(MealTime.LUNCH.name())) {
+            return MealTime.LUNCH;
+        } else if(mealTime.contains(MealTime.DINNER.name())) {
+            return MealTime.DINNER;
+        } else {
+            throw new RuntimeException();
+        }
     }
 
-    // TODO: implement enum input type
-    public void setMealTime( String mealTime) {
-        MealTime = mealTime;
+    public void setMealTime(MealTime mealTime) {
+        this.mealTime = mealTime.name();
     }
 
-    // TODO: implement enum return type
-    public String getEntryType() {
-        return EntryType;
+    public EntryType getEntryType() {
+        if(entryType.contains(EntryType.DEFAULT.name())) {
+            return EntryType.DEFAULT;
+        } else if(entryType.contains(EntryType.CUSTOM.name())) {
+            return EntryType.CUSTOM;
+        } else {
+            throw new RuntimeException();
+        }
     }
 
-    // TODO: implement enum input type
-    public void setEntryType(String entryType) {
-        EntryType = entryType;
+    public void setEntryType(EntryType entryType) {
+        this.entryType = entryType.name();
     }
 }
