acd5089e8533e378ba668535ea9aba660a43bc31
knarjp <kevajess@gmail.com>
2018-03-24 12:58:28 -0400

    first version of meal generator tested and implemented

--- app/src/androidTest/java/com/design/senior/what2eat/MealGeneratorInstrumentedTests.java
+++ app/src/androidTest/java/com/design/senior/what2eat/MealGeneratorInstrumentedTests.java
@@ -1,9 +1,14 @@
 package com.design.senior.what2eat;
 
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
+import com.design.senior.what2eat.DatabaseComponents.Entities.Entry;
+import com.design.senior.what2eat.DatabaseComponents.Entities.Meal;
+import com.design.senior.what2eat.DatabaseComponents.Entities.MealEntryJoin;
 import com.design.senior.what2eat.DatabaseComponents.Enums.AllergyType;
 import com.design.senior.what2eat.DatabaseComponents.Enums.DietType;
+import com.design.senior.what2eat.DatabaseComponents.Enums.MealTime;
 import com.design.senior.what2eat.MealGenerators.MealGenerator;
 
 import org.junit.Assert;
@@ -11,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,6 +25,7 @@ import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 public class MealGeneratorInstrumentedTests {
+
     @Test
     public void testMealGeneratorSetters() throws Exception {
         MealGenerator generator = new MealGenerator();
@@ -89,7 +96,7 @@ public class MealGeneratorInstrumentedTests {
         Assert.assertFalse(generator.getDisallowedAllergies().contains(AllergyType.SOY));
         Assert.assertFalse(generator.getDisallowedAllergies().contains(AllergyType.TREENUTS));
         Assert.assertFalse(generator.getDisallowedAllergies().contains(AllergyType.WHEAT));
-        
+
         diets.clear();
         allergyTypes.clear();
 
@@ -111,5 +118,166 @@ public class MealGeneratorInstrumentedTests {
         Assert.assertFalse(generator.getDisallowedAllergies().contains(AllergyType.SOY));
         Assert.assertFalse(generator.getDisallowedAllergies().contains(AllergyType.TREENUTS));
         Assert.assertFalse(generator.getDisallowedAllergies().contains(AllergyType.WHEAT));
+    }
+
+    @Test
+    public void testMealGeneratorGeneration() throws Exception {
+        MealGenerator generator = new MealGenerator();
+        generator.setCalorieTarget(2150);
+
+        Meal breakfast1 = new Meal();
+        breakfast1.setMealID(0);
+        breakfast1.setCaloricAmount(500);
+        breakfast1.setMealTimeEnum(MealTime.BREAKFAST);
+
+        Meal breakfast2 = new Meal();
+        breakfast2.setMealID(1);
+        breakfast2.setCaloricAmount(250);
+        breakfast2.setMealTimeEnum(MealTime.BREAKFAST);
+
+        Meal breakfast3 = new Meal();
+        breakfast3.setMealID(2);
+        breakfast3.setCaloricAmount(800);
+        breakfast3.setMealTimeEnum(MealTime.BREAKFAST);
+
+        Meal breakfast4 = new Meal();
+        breakfast4.setMealID(3);
+        breakfast4.setCaloricAmount(425);
+        breakfast4.setMealTimeEnum(MealTime.BREAKFAST);
+
+        Meal breakfast5 = new Meal();
+        breakfast5.setMealID(4);
+        breakfast5.setCaloricAmount(100);
+        breakfast5.setMealTimeEnum(MealTime.BREAKFAST);
+
+        Meal breakfast6 = new Meal();
+        breakfast6.setMealID(5);
+        breakfast6.setCaloricAmount(335);
+        breakfast6.setMealTimeEnum(MealTime.BREAKFAST);
+
+        Meal lunch1 = new Meal();
+        lunch1.setMealID(6);
+        lunch1.setCaloricAmount(125);
+        lunch1.setMealTimeEnum(MealTime.LUNCH);
+
+        Meal lunch2 = new Meal();
+        lunch2.setMealID(7);
+        lunch2.setCaloricAmount(250);
+        lunch2.setMealTimeEnum(MealTime.LUNCH);
+
+        Meal lunch3 = new Meal();
+        lunch3.setMealID(8);
+        lunch3.setCaloricAmount(650);
+        lunch3.setMealTimeEnum(MealTime.LUNCH);
+
+        Meal lunch4 = new Meal();
+        lunch4.setMealID(9);
+        lunch4.setCaloricAmount(725);
+        lunch4.setMealTimeEnum(MealTime.LUNCH);
+
+        Meal lunch5 = new Meal();
+        lunch5.setMealID(10);
+        lunch5.setCaloricAmount(480);
+        lunch5.setMealTimeEnum(MealTime.LUNCH);
+
+        Meal lunch6 = new Meal();
+        lunch6.setMealID(11);
+        lunch6.setCaloricAmount(100);
+        lunch6.setMealTimeEnum(MealTime.LUNCH);
+
+        Meal dinner1 = new Meal();
+        dinner1.setMealID(12);
+        dinner1.setCaloricAmount(1100);
+        dinner1.setMealTimeEnum(MealTime.DINNER);
+
+        Meal dinner2 = new Meal();
+        dinner2.setMealID(13);
+        dinner2.setCaloricAmount(440);
+        dinner2.setMealTimeEnum(MealTime.DINNER);
+
+        Meal dinner3 = new Meal();
+        dinner3.setMealID(14);
+        dinner3.setCaloricAmount(670);
+        dinner3.setMealTimeEnum(MealTime.DINNER);
+
+        Meal dinner4 = new Meal();
+        dinner4.setMealID(15);
+        dinner1.setCaloricAmount(850);
+        dinner1.setMealTimeEnum(MealTime.DINNER);
+
+        Meal dinner5 = new Meal();
+        dinner5.setMealID(16);
+        dinner1.setCaloricAmount(425);
+        dinner1.setMealTimeEnum(MealTime.DINNER);
+
+        Meal dinner6 = new Meal();
+        dinner6.setMealID(17);
+        dinner1.setCaloricAmount(375);
+        dinner1.setMealTimeEnum(MealTime.DINNER);
+
+        List<Meal> mealList = new ArrayList<>();
+        mealList.add(breakfast1);
+        mealList.add(breakfast2);
+        mealList.add(breakfast3);
+        mealList.add(breakfast4);
+        mealList.add(breakfast5);
+        mealList.add(breakfast6);
+        mealList.add(lunch1);
+        mealList.add(lunch2);
+        mealList.add(lunch3);
+        mealList.add(lunch4);
+        mealList.add(lunch5);
+        mealList.add(lunch6);
+        mealList.add(dinner1);
+        mealList.add(dinner2);
+        mealList.add(dinner3);
+        mealList.add(dinner4);
+        mealList.add(dinner5);
+        mealList.add(dinner6);
+
+        Entry date1 = new Entry();
+        date1.setID(0);
+
+        Entry date2 = new Entry();
+        date2.setID(1);
+
+        List<Entry> entryList = new ArrayList<>();
+        entryList.add(date1);
+        entryList.add(date2);
+
+        List<MealEntryJoin> generatedMeals;
+
+        generatedMeals = generator.generateMeals(mealList, entryList);
+
+        Assert.assertFalse(generatedMeals.isEmpty());
+
+        List<Meal> date1Meals = new ArrayList<>();
+        List<Meal> date2Meals = new ArrayList<>();
+
+        for(MealEntryJoin tuple : generatedMeals) {
+            for (Meal meal : mealList) {
+                if (tuple.getMeal() == meal.getMealID()) {
+                    if(tuple.getEntry() == date1.getID()) {
+                        date1Meals.add(meal);
+                    } else if (tuple.getEntry() == date2.getID()) {
+                        date2Meals.add(meal);
+                    }
+                }
+            }
+        }
+
+        int date1Calories = 0;
+        int date2Calories = 0;
+
+        for(Meal meal : date1Meals) {
+            date1Calories += meal.getCaloricAmount();
+        }
+
+        for(Meal meal : date2Meals) {
+            date2Calories += meal.getCaloricAmount();
+        }
+
+        Assert.assertEquals(2150, date1Calories, 150); // assert true if calories between 2000 and 2300
+        Assert.assertEquals(2150, date2Calories, 150); // assert true if calories between 2000 and 2300
     }
 }
--- app/src/main/java/com/design/senior/what2eat/MealGenerators/MealGenerator.java
+++ app/src/main/java/com/design/senior/what2eat/MealGenerators/MealGenerator.java
@@ -1,13 +1,16 @@
 package com.design.senior.what2eat.MealGenerators;
 
+import android.util.Log;
+
+import com.design.senior.what2eat.DatabaseComponents.Entities.Entry;
 import com.design.senior.what2eat.DatabaseComponents.Entities.Meal;
 import com.design.senior.what2eat.DatabaseComponents.Entities.MealEntryJoin;
 import com.design.senior.what2eat.DatabaseComponents.Enums.AllergyType;
 import com.design.senior.what2eat.DatabaseComponents.Enums.DietType;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by KJ on 3/24/2018.
@@ -29,26 +32,164 @@ public class MealGenerator {
         allowedDiets.add(DietType.NONE);
     }
 
-    public List<MealEntryJoin> generateMeals(List<Meal> availableMeals, Date startDate) {
+    public List<MealEntryJoin> generateMeals(List<Meal> mealsToUse, List<Entry> datesToUse) {
         List<MealEntryJoin> generatedMealsList = new ArrayList<>();
 
-        List<Meal> allowedMeals = new ArrayList<>();
+        List<Meal> allowedBreakfasts = new ArrayList<>();
+        List<Meal> allowedLunches = new ArrayList<>();
+        List<Meal> allowedDinners = new ArrayList<>();
 
-       /* for(Meal meal : availableMeals) {
-
-            allowedMeals.add(meal);
-
-            if(!allowedDiets.contains(meal.getDietTypeEnum())) {
-                allowedMeals.remove(meal);
+        for(Meal mealCandidate : mealsToUse) {
+            switch(mealCandidate.getMealTimeEnum()) {
+                case BREAKFAST:
+                    allowedBreakfasts.add(mealCandidate);
+                    break;
+                case LUNCH:
+                    allowedLunches.add(mealCandidate);
+                    break;
+                case DINNER:
+                    allowedDinners.add(mealCandidate);
+                    break;
+                default:
+                    throw new RuntimeException();
             }
 
-            for(AllergyType allergy : meal.getAllergiesEnum()) {
-                if(disallowedAllergies.contains(allergy)) {
-
+            if(!disallowedAllergies.contains(AllergyType.NONE)) {
+                for (AllergyType badAllergy : disallowedAllergies) {
+                    if (mealCandidate.getAllergiesEnum().contains(badAllergy)) {
+                        switch(mealCandidate.getMealTimeEnum()) {
+                            case BREAKFAST:
+                                allowedBreakfasts.remove(mealCandidate);
+                                break;
+                            case LUNCH:
+                                allowedLunches.remove(mealCandidate);
+                                break;
+                            case DINNER:
+                                allowedDinners.remove(mealCandidate);
+                                break;
+                            default:
+                                throw new RuntimeException();
+                        }
+                        break;
+                    }
                 }
             }
-        }*/
 
+            if(!allowedDiets.contains(DietType.NONE) && !allowedDiets.contains(mealCandidate.getDietTypeEnum())) {
+                switch(mealCandidate.getMealTimeEnum()) {
+                    case BREAKFAST:
+                        allowedBreakfasts.remove(mealCandidate);
+                        break;
+                    case LUNCH:
+                        allowedLunches.remove(mealCandidate);
+                        break;
+                    case DINNER:
+                        allowedDinners.remove(mealCandidate);
+                        break;
+                    default:
+                        throw new RuntimeException();
+                }
+                break;
+            }
+        }
+
+        for(Entry day : datesToUse) {
+            int dayID = day.getID();
+            int caloriesLeft = calorieTarget;
+
+            Random rand = new Random();
+
+            boolean hasNoBreakfast = true;
+            boolean hasNoLunch = true;
+            boolean hasNoDinner = true;
+
+            while(hasNoBreakfast) {
+                Meal meal = allowedBreakfasts.get(rand.nextInt(allowedBreakfasts.size()));
+
+                if(meal.getCaloricAmount() < caloriesLeft/3 + 150) {
+                    caloriesLeft -= meal.getCaloricAmount();
+
+                    generatedMealsList.add(new MealEntryJoin(meal.getMealID(), dayID));
+
+                    hasNoBreakfast = false;
+                }
+            }
+
+            while(hasNoLunch) {
+                Meal meal = allowedLunches.get(rand.nextInt(allowedLunches.size()));
+
+                if(meal.getCaloricAmount() < caloriesLeft/3 + 150) {
+                    caloriesLeft -= meal.getCaloricAmount();
+
+                    generatedMealsList.add(new MealEntryJoin(meal.getMealID(), dayID));
+
+                    hasNoLunch = false;
+                }
+            }
+
+            while(hasNoDinner) {
+                Meal meal = allowedDinners.get(rand.nextInt(allowedDinners.size()));
+
+                if(meal.getCaloricAmount() < caloriesLeft/3 + 150) {
+                    caloriesLeft -= meal.getCaloricAmount();
+
+                    generatedMealsList.add(new MealEntryJoin(meal.getMealID(), dayID));
+
+                    hasNoDinner = false;
+                }
+            }
+
+            while(!(caloriesLeft < 150) && !(caloriesLeft < -150)) {
+                Random randomTime = new Random();
+                switch(randomTime.nextInt(4)) {
+                    case 1:
+                        hasNoBreakfast = true;
+
+                        while(hasNoBreakfast) {
+                            Meal meal = allowedBreakfasts.get(rand.nextInt(allowedBreakfasts.size()));
+
+                            if(meal.getCaloricAmount() < caloriesLeft + 150) {
+                                caloriesLeft -= meal.getCaloricAmount();
+
+                                generatedMealsList.add(new MealEntryJoin(meal.getMealID(), dayID));
+
+                                hasNoBreakfast = false;
+                            }
+                        }
+                        break;
+                    case 2:
+                        hasNoLunch = true;
+
+                        while(hasNoLunch) {
+                            Meal meal = allowedLunches.get(rand.nextInt(allowedLunches.size()));
+
+                            if(meal.getCaloricAmount() < caloriesLeft + 150) {
+                                caloriesLeft -= meal.getCaloricAmount();
+
+                                generatedMealsList.add(new MealEntryJoin(meal.getMealID(), dayID));
+
+                                hasNoLunch = false;
+                            }
+                        }
+                        break;
+                    case 3:
+                        hasNoDinner = true;
+
+                        while(hasNoDinner) {
+                            Meal meal = allowedDinners.get(rand.nextInt(allowedDinners.size()));
+
+                            if(meal.getCaloricAmount() < caloriesLeft + 150) {
+                                caloriesLeft -= meal.getCaloricAmount();
+
+                                generatedMealsList.add(new MealEntryJoin(meal.getMealID(), dayID));
+
+                                hasNoDinner = false;
+                            }
+                        }
+                        break;
+                }
+            }
+        }
 
         return generatedMealsList;
     }
