4ee9315b65b1c065b661a095f55caa1e5389466a
knarjp <kevajess@gmail.com>
2018-03-24 20:35:03 -0400

    added a bunch of buttons for generator options

--- app/src/main/java/com/design/senior/what2eat/Fragments/CalendarOptionsFragment.java
+++ app/src/main/java/com/design/senior/what2eat/Fragments/CalendarOptionsFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import com.design.senior.what2eat.R;
 
@@ -15,7 +18,19 @@ import com.design.senior.what2eat.R;
 
 public class CalendarOptionsFragment extends Fragment {
 
-    private TextView helloText;
+    private TextView allergiesText;
+    private TextView dietText;
+
+    private CheckBox milkCheck;
+    private CheckBox eggsCheck;
+    private CheckBox fishCheck;
+    private CheckBox shellfishCheck;
+    private CheckBox treenutsCheck;
+    private CheckBox peanutsCheck;
+    private CheckBox wheatCheck;
+    private CheckBox soyCheck;
+
+    private RadioGroup radioGroup;
 
     public CalendarOptionsFragment() {
         // Required empty public constructor
@@ -34,7 +49,21 @@ public class CalendarOptionsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_calendar_options, container, false);
 
         // assign view fields to xml IDs
-        helloText = (TextView) view.findViewById(R.id.testText);
+        allergiesText = (TextView) view.findViewById(R.id.options_allergies_header);
+        dietText = (TextView) view.findViewById(R.id.options_diets_header);
+
+        milkCheck = (CheckBox) view.findViewById(R.id.allergies_milk_checkbox);
+        eggsCheck = (CheckBox) view.findViewById(R.id.allergies_eggs_checkbox);
+        fishCheck = (CheckBox) view.findViewById(R.id.allergies_fish_checkbox);
+        shellfishCheck = (CheckBox) view.findViewById(R.id.allergies_shellfish_checkbox);
+        treenutsCheck = (CheckBox) view.findViewById(R.id.allergies_treenuts_checkbox);
+        peanutsCheck = (CheckBox) view.findViewById(R.id.allergies_peanuts_checkbox);
+        wheatCheck = (CheckBox) view.findViewById(R.id.allergies_wheat_checkbox);
+        soyCheck = (CheckBox) view.findViewById(R.id.allergies_soy_checkbox);
+
+        radioGroup = (RadioGroup) view.findViewById(R.id.options_radio_group);
+
+        // TODO: set onclick listeners
 
         return view;
     }
